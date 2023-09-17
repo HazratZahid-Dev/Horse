2afe104ea7e01a29a0c8dd5aef0036e3fd810c2c
HazratZahid9454 <zahid454688@gmail.com>
2023-09-17 17:58:36 +0500

    change

---
+++ HorseLux/src/Compunents/SelectHorse.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SelectHorse = () => {
+  return (
+    <div>SelectHorse</div>
+  )
+}
+
+export default SelectHorse
--- HorseLux/src/Pages/Report/HorseReport.jsx
+++ HorseLux/src/Pages/Report/HorseReport.jsx
@@ -6,6 +6,7 @@ import dateTo from '../../Images/reportImage/DateTo.png'
 import temp from '../../Images/reportImage/Temperature.png'
 import user from '../../Images/reportImage/User.png'
 import barley from '../../Images/reportImage/Barley.png'
+import { Link } from 'react-router-dom'
 
 
 
@@ -20,24 +21,24 @@ const HorseReport = () => {
         <h2 className='text-[35px] font-[700] leading-[45px]'>Horse Report</h2>
         <hr/>
         <div className='w-[30%] mt-5 flex items-center justify-between flex-wrap  gap-6'>
-        <div className='border hover:border-gray-400 w-[140px] hover:scale-105 cursor-pointer flex items-center justify-center shadow-xl h-[150px] rounded-[18px]'>
+        <Link className='border hover:border-gray-400 w-[140px] hover:scale-105 cursor-pointer flex items-center justify-center shadow-xl h-[150px] rounded-[18px]'>
            <img src={dTime} alt='dTime' className='w-[63px] hover:scale-110 h-[63px]'/>
-        </div>
-        <div className='border hover:border-gray-400 w-[140px] hover:scale-105 cursor-pointer flex items-center justify-center shadow-xl h-[150px] rounded-[18px]'>
+        </Link>
+        <Link className='border hover:border-gray-400 w-[140px] hover:scale-105 cursor-pointer flex items-center justify-center shadow-xl h-[150px] rounded-[18px]'>
            <img src={heart} alt='dTime' className='w-[63px] hover:scale-110 h-[63px]'/>
-        </div>
-        <div className='border hover:border-gray-400 w-[140px] hover:scale-105 cursor-pointer flex items-center justify-center shadow-xl h-[150px] rounded-[18px]'>
+        </Link>
+        <Link className='border hover:border-gray-400 w-[140px] hover:scale-105 cursor-pointer flex items-center justify-center shadow-xl h-[150px] rounded-[18px]'>
            <img src={dateTo} alt='dTime' className='w-[63px] hover:scale-110 h-[63px]'/>
-        </div>
-        <div className='border hover:border-gray-400 w-[140px] hover:scale-105 cursor-pointer flex items-center justify-center shadow-xl h-[150px] rounded-[18px]'>
+        </Link>
+        <Link className='border hover:border-gray-400 w-[140px] hover:scale-105 cursor-pointer flex items-center justify-center shadow-xl h-[150px] rounded-[18px]'>
            <img src={temp} alt='dTime' className='w-[63px] hover:scale-110 h-[63px]'/>
-        </div>
-        <div className='border hover:border-gray-400 w-[140px] hover:scale-105 cursor-pointer flex items-center justify-center shadow-xl h-[150px] rounded-[18px]'>
+        </Link>
+        <Link className='border hover:border-gray-400 w-[140px] hover:scale-105 cursor-pointer flex items-center justify-center shadow-xl h-[150px] rounded-[18px]'>
            <img src={user} alt='dTime' className='w-[63px] hover:scale-110 h-[63px]'/>
-        </div>
-        <div className='border hover:border-gray-400 w-[140px] hover:scale-105  cursor-pointer flex items-center justify-center shadow-xl h-[150px] rounded-[18px]'>
+        </Link>
+        <Link className='border hover:border-gray-400 w-[140px] hover:scale-105  cursor-pointer flex items-center justify-center shadow-xl h-[150px] rounded-[18px]'>
            <img src={barley} alt='dTime' className='w-[63px] hover:scale-110 h-[63px]'/>
-        </div>
+        </Link>
 
 
         </div>
